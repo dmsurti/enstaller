bb6ba74d0eb5895d26cd464e634b6ec32a77f36b
Michael Gummelt <mgummelt@enthought.com>
2013-11-13 11:27:15 -0600

    copy over GritsClientStore.  remove grits_client dependency

--- enstaller/store/grits_egg_store.py
+++ enstaller/store/grits_egg_store.py
@@ -1,6 +1,77 @@
+import requests
+import json
+
+from encore.storage.dynamic_url_store import DynamicURLStore
+
 from enstaller import plat
 
-from grits_client.storage.client_store import GritsClientStore
+class GritsClientStore(DynamicURLStore):
+    ''' Copied from grits_client.storage.client_store '''
+    def __init__(self, url):
+        self.url = clean_url(url)
+        base_url = self.url + '/store'
+        super(GritsClientStore, self).__init__(base_url, base_url)
+        self.authenticated = False
+
+    def connect(self, creds):
+        self._session = requests.Session()
+
+        if creds is not None:
+            username, password = creds
+            response = self._session.post(self.url + '/authenticate',
+                                          data={'email': username,
+                                                'password':password})
+
+            response.raise_for_status()
+            self.user_data = response.json()
+            self._user_tag = self.user_data['user_tag']
+            self.authenticated = True
+
+        self._connected = True
+
+    def is_connected(self):
+        return self._connected
+
+    def add_action(self, user_id, action_type, action_time, **details):
+        action_dict = {"user_id": user_id,
+                       "action_type": action_type,
+                       "action_time": action_time,
+                       "details": json.dumps(details)}
+
+        self._session.post(self.url + '/action',
+                           data=action_dict) \
+                     .raise_for_status()
+
+    def query(self, select=None, since=None, **kwargs):
+        params = {key: json.dumps(value) for key, value in kwargs.items()}
+        params['_with_metadata'] = 'true'
+        headers = {'Content-type': 'application/json'}
+        if since:
+            headers['If-Modified-Since'] = formatdate(since)
+
+        response = self._session.get(self.query_url, headers=headers, params=params)
+        response.raise_for_status()
+
+        body = response.json()
+        if select:
+            ret = {}
+            for key, metadata in body.iteritems():
+                ret[key] = {}
+                for k, v in metadata.iteritems():
+                    if k in select:
+                        ret[key][k] = v
+            return ret
+        else:
+            return body.items()
+
+    @property
+    def user_store(self):
+        return self.user_data['stores'][0]
+
+    @property
+    def user_spaces(self):
+        return self.user_store['user_spaces']
+
 
 class GritsEggStore(GritsClientStore):
     def __init__(self, url):
